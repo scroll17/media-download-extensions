53bb6f40ae965e56cca283d5327689628557890e
scroll17 <47834834+scroll17@users.noreply.github.com>
2019-10-26 11:49:25 +0300

    26/10/2019 : 12:00

--- loadVideo.js
+++ loadVideo.js
@@ -1,7 +1,5 @@
 const createVideoUrl = (url) => `https://www.instagram.com/graphql/query/?query_hash=870ea3e846839a3b6a8cd9cd7e42290c&variables=%7B%22shortcode%22%3A%22${url}%22%2C%22child_comment_count%22%3A3%2C%22fetch_comment_count%22%3A40%2C%22parent_comment_count%22%3A24%2C%22has_threaded_comments%22%3Atrue%7D`;
 
-const videoLocationPathname = document.location.pathname.match(/\/p\/(.*)\//)[1];
-
 const createLink = (src) => {
     const link = document.createElement('a');
     link.setAttribute('target', "__blank");
@@ -24,15 +22,17 @@ const getResponseObject = (url) => {
     return responseObject.data.shortcode_media.video_url;
 };
 
+
 function donwloadStory(){
-    return () => {
-        const video = document.getElementsByClassName('y-yJ5 OFkrO') [0].getElementsByTagName("source")[0];
-        createLink(video.src);
-    }
+    const video = document.getElementsByClassName('y-yJ5 OFkrO') [0].getElementsByTagName("source")[0];
+    createLink(video.src);
 };
 
 const donwloadVideo = () => {
+    const videoLocationPathname = document.location.pathname.match(/\/p\/(.*)\//)[1];
+
     const url = createVideoUrl(videoLocationPathname);
+
     const src = getResponseObject(url);
 
     createLink(src);
